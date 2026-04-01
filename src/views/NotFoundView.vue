69837ed67f17a37c40d832d51add2c00ea5f5046
a-oirgari <abdrahmanghali1998@gmail.com>
2026-04-01 19:49:13 +0100

    feat: the notfoundView file

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="auth-page">
+    <div class="auth-card" style="text-align:center">
+      <h1 style="font-size:4rem;margin:0">404</h1>
+      <p>Cette page n'existe pas.</p>
+      <RouterLink to="/login" class="btn-link">Retour à l'accueil</RouterLink>
+    </div>
+  </div>
+</template>
